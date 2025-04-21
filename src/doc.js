f9d6ac30f8c3658a4bdb8e137bc15c85a979ab73
Jenkins <Jenkins>
2025-04-21 13:26:38 +0000

    Demo update v1.1.7

--- src/doc.js
+++ src/doc.js
@@ -14,146 +14,145 @@ const saveButton = document.getElementById('doc-save-button');
 const resetCameraButton = document.getElementById('reset-camera-button');
 
 const setUIValues = (id, value) => {
-    const el = document.getElementById(id);
-    el.value = value;
+  const el = document.getElementById(id);
+  el.value = value;
 };
 
 const onOpened = () => {
-    console.log('opened');
-    menuButtons.style.display = 'none';
+  console.log('opened');
+  menuButtons.style.display = 'none';
 };
 
 const onClosed = () => {
-    console.log('closed');
-    liveDocumentCamera.style.display = 'none';
-    if (modal.style.display === 'none' && documentResult.style.display === 'none') {
-        menuButtons.style.display = 'flex';
-    }
+  console.log('closed');
+  liveDocumentCamera.style.display = 'none';
+  if (modal.style.display === 'none' && documentResult.style.display === 'none') {
+    menuButtons.style.display = 'flex';
+  }
 };
 
 const resolveCheckStatus = (status) => {
-    if (status == null) {
-        return 'N/A';
-    }
-    return (status ? 'OK' : 'Failed');
+  if (status == null) {
+    return 'N/A';
+  }
+  return (status ? 'OK' : 'Failed');
 };
 
 const onCaptured = (e) => {
-    console.log('captured');
-    const { captureResponse } = e.detail;
-    let baseStr = 'NA';
-    // Hide live document camera and update UI state
-    liveDocumentCamera.style.display = 'none';
-    liveDocumentCamera.isOpen = false;
-    if (captureResponse.imageData) {
-      // Show document result
-      documentResult.style.display = 'block';
+  console.log('captured');
+  const { captureResponse } = e.detail;
+  let baseStr = 'NA';
+  // Hide live document camera and update UI state
+  liveDocumentCamera.style.display = 'none';
+  liveDocumentCamera.isOpen = false;
+  if (captureResponse.imageData) {
+    // Show document result
+    documentResult.style.display = 'block';
 
-      // Get canvas context and set dimensions based on the image data
-      const documentContext = documentCanvas.getContext('2d');
-      documentCanvas.height = captureResponse.imageHeight;
-      documentCanvas.width = captureResponse.imageWidth;
+    // Get canvas context and set dimensions based on the image data
+    const documentContext = documentCanvas.getContext('2d');
+    documentCanvas.height = captureResponse.imageHeight;
+    documentCanvas.width = captureResponse.imageWidth;
 
-      // Draw the ImageData onto the canvas
-      documentContext.putImageData(captureResponse.imageData, 0, 0);
+    // Draw the ImageData onto the canvas
+    documentContext.putImageData(captureResponse.imageData, 0, 0);
 
-      // Create an off-screen canvas to convert ImageData to base64
-      const offScreenCanvas = document.createElement('canvas');
-      const offScreenContext = offScreenCanvas.getContext('2d');
+    // Create an off-screen canvas to convert ImageData to base64
+    const offScreenCanvas = document.createElement('canvas');
+    const offScreenContext = offScreenCanvas.getContext('2d');
 
-      // Set the off-screen canvas dimensions to match the image data
-      offScreenCanvas.width = captureResponse.imageWidth;
-      offScreenCanvas.height = captureResponse.imageHeight;
+    // Set the off-screen canvas dimensions to match the image data
+    offScreenCanvas.width = captureResponse.imageWidth;
+    offScreenCanvas.height = captureResponse.imageHeight;
 
-      // Draw the ImageData onto the off-screen canvas
-      offScreenContext.putImageData(captureResponse.imageData, 0, 0);
+    // Draw the ImageData onto the off-screen canvas
+    offScreenContext.putImageData(captureResponse.imageData, 0, 0);
 
-      // Convert the off-screen canvas to base64
-      const base64Image = offScreenCanvas.toDataURL('image/png');
-      baseStr = base64Image;
-      baseStr = baseStr.replace(/^data:image\/png;base64,/, '');
+    // Convert the off-screen canvas to base64
+    const base64Image = offScreenCanvas.toDataURL('image/png');
+    baseStr = base64Image;
+    baseStr = baseStr.replace(/^data:image\/png;base64,/, '');
 
-    } else {
-      // Show menu buttons if no image data is present
-      menuButtons.style.display = 'flex';
-    }
+  } else {
+    // Show menu buttons if no image data is present
+    menuButtons.style.display = 'flex';
+  }
 
-    // Set various UI values based on captureResponse
-    setUIValues('base64-img-input', baseStr);
+  // Set various UI values based on captureResponse
+  setUIValues('base64-img-input', baseStr);
 
-    setUIValues('doc-is-good-input', captureResponse.isGood ? 'OK' : 'Failed');
-    setUIValues('doc-sharpness-input', resolveCheckStatus(captureResponse.isSharp));
-    setUIValues('doc-glare-input', resolveCheckStatus(captureResponse.isGlareFree));
-    setUIValues('doc-dpi-input', resolveCheckStatus(captureResponse.isAdequateDpi));
+  setUIValues('doc-is-good-input', captureResponse.isGood ? 'OK' : 'Failed');
+  setUIValues('doc-sharpness-input', resolveCheckStatus(captureResponse.isSharp));
+  setUIValues('doc-glare-input', resolveCheckStatus(captureResponse.isGlareFree));
+  setUIValues('doc-dpi-input', resolveCheckStatus(captureResponse.isAdequateDpi));
 
-    if (captureResponse.failedChecks) {
-      setUIValues('doc-failed-checks-input', captureResponse.failedChecks.length && `${captureResponse.failedChecks.map(checkName => checkName)}`);
-    }
-  };
+  if (captureResponse.failedChecks) {
+    setUIValues('doc-failed-checks-input', captureResponse.failedChecks.length && `${captureResponse.failedChecks.map(checkName => checkName)}`);
+  }
+};
 
 
 const onFailure = (e) => {
-    const {error} = e.detail;
-    console.log(error.code, error.message);
-    menuButtons.style.display = 'none';
-    modal.style.display = 'flex';
-    modalText.innerHTML = error.message;
-    modalButton.addEventListener('click', openManualCamera);
+  const { error } = e.detail;
+  console.log(error.code, error.message);
+  menuButtons.style.display = 'none';
+  modal.style.display = 'flex';
+  modalText.innerHTML = error.message;
+  modalButton.addEventListener('click', openManualCamera);
 };
 
 const openLiveCamera = () => {
-    navigator.mediaDevices.getUserMedia({ video: true })
+  navigator.mediaDevices.getUserMedia({ video: true })
     .then((stream) => {
-        console.log('Camera access granted');
-        liveDocumentCamera.setShowBackOfDocumentAnimation(false);
+      console.log('Camera access granted');
 
-        liveDocumentCamera.forceManualCamera = false;
-        menuButtons.style.display = 'none';
-        liveDocumentCamera.style.display = 'block';
-        liveDocumentCamera.isOpen = true;
-        errorCamera.style.display = 'none';
-        // Stop the stream after getting permission
-        stream.getTracks().forEach(track => track.stop());
+      liveDocumentCamera.forceManualCamera = false;
+      menuButtons.style.display = 'none';
+      liveDocumentCamera.style.display = 'block';
+      liveDocumentCamera.isOpen = true;
+      errorCamera.style.display = 'none';
+      // Stop the stream after getting permission
+      stream.getTracks().forEach(track => track.stop());
     })
     .catch((err) => {
-        console.error('Error accessing the camera:', err);
-        errorCamera.style.display = 'flex';
+      console.error('Error accessing the camera:', err);
+      errorCamera.style.display = 'flex';
     });
 };
 
 
 const saveImage = () => {
-    const link = document.createElement('a');
-    document.body.appendChild(link); // for Firefox
-    const dataURL = documentCanvas.toDataURL('image/png');
+  const link = document.createElement('a');
+  document.body.appendChild(link); // for Firefox
+  const dataURL = documentCanvas.toDataURL('image/png');
 
-    link.setAttribute('href', dataURL);
-    link.setAttribute('download', `${Date.now()}.jpg`);
-    link.click();
+  link.setAttribute('href', dataURL);
+  link.setAttribute('download', `${Date.now()}.jpg`);
+  link.click();
 
-    document.body.removeChild(link);
+  document.body.removeChild(link);
 };
 
 const resetCamera = () => {
   documentResult.style.display = 'none';
   liveDocumentCamera.style.display = 'block';
   liveDocumentCamera.isOpen = true;
-  liveDocumentCamera.setShowBackOfDocumentAnimation(true);
+  if (liveDocumentCamera.showBackOfDocumentAnimation) liveDocumentCamera.showBackOfDocumentAnimation = true;
 };
 
 const setupDocCamera = () => {
 
-    SmartCaptureModule.getInstance().init();
-    liveDocumentCaptureButton.addEventListener('click', openLiveCamera);
-    saveButton.addEventListener('click', saveImage);
-    resetCameraButton.addEventListener('click', resetCamera);
-    liveDocumentCamera.addEventListener(LiveDocumentCamera.OpenEventName, onOpened);
-    liveDocumentCamera.addEventListener(LiveDocumentCamera.CloseEventName, onClosed);
-    liveDocumentCamera.addEventListener(LiveDocumentCamera.FailureEventName, onFailure);
-    liveDocumentCamera.addEventListener(LiveDocumentCamera.CaptureEventName, onCaptured);
-    loader.style.display = 'none';
-    modal.style.display = 'none';
-    menuButtons.style.display = 'flex';
+  SmartCaptureModule.getInstance().init();
+  liveDocumentCaptureButton.addEventListener('click', openLiveCamera);
+  saveButton.addEventListener('click', saveImage);
+  resetCameraButton.addEventListener('click', resetCamera);
+  liveDocumentCamera.addEventListener(LiveDocumentCamera.OpenEventName, onOpened);
+  liveDocumentCamera.addEventListener(LiveDocumentCamera.CloseEventName, onClosed);
+  liveDocumentCamera.addEventListener(LiveDocumentCamera.FailureEventName, onFailure);
+  liveDocumentCamera.addEventListener(LiveDocumentCamera.CaptureEventName, onCaptured);
+  loader.style.display = 'none';
+  modal.style.display = 'none';
+  menuButtons.style.display = 'flex';
 };
 
 setupDocCamera();
