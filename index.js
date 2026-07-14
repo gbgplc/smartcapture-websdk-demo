cf65a3a09b968d0738be94a50b923bf8366ecfdf
Jenkins <Jenkins>
2026-07-14 15:46:00 +0000

    Demo update v2.2.3

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const basePath = `${window.location.origin}${window.location.pathname.replace(/\
 
 const configure = () => {
   const documentCamera = document.createElement('script');
-  documentCamera.src = `${basePath}/src/doc.js`
+  documentCamera.src = `${basePath}/src/doc.js`;
   documentCamera.type = 'module';
   document.body.appendChild(documentCamera);
 
   const face = document.createElement('script');
-  face.src = `${basePath}/src/face.js`
+  face.src = `${basePath}/src/face.js`;
   face.type = 'module';
   document.body.appendChild(face);
 };
